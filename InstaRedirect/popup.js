e513abf86f6f4bcaefe91d42057e6f655ddf38d7
v1p3rrrrr <45924304+v1p3rrrrr@users.noreply.github.com>
2023-05-26 17:28:36 +0300

    Update popup.js

--- InstaRedirect/popup.js
+++ InstaRedirect/popup.js
@@ -15,11 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
       setActiveOption("picuki");
     });
   
-    // Retrieve the current selected option from storage and update the UI
     chrome.storage.sync.get("redirectOption", function(data) {
       var redirectOption = data.redirectOption;
       if (!redirectOption) {
-        redirectOption = null; // Set "disabled" as the default option
+        redirectOption = null; 
         chrome.storage.sync.set({ redirectOption: redirectOption });
       }
       updateActiveOption(redirectOption);
@@ -28,18 +27,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function setActiveOption(option) {
       chrome.storage.sync.set({ redirectOption: option }, function() {
         updateActiveOption(option);
-        // Send the selected option to background.js
         chrome.runtime.sendMessage({ option: option });
       });
     }
   
     function updateActiveOption(redirectOption) {
-      // Remove the "selected" class from all buttons
       disabledButton.classList.remove("selected");
       redirectDumporButton.classList.remove("selected");
       redirectPicukiButton.classList.remove("selected");
   
-      // Set the "selected" class to the corresponding button based on the redirectOption
       if (redirectOption === null) {
         disabledButton.classList.add("selected");
       } else if (redirectOption === "dumpor") {
